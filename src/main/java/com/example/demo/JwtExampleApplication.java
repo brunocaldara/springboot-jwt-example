6d2779862f390f9bd5a40c2b79d130075c4a5c8b
Bruno Caldara <bruno@localhost.localdomain>
2023-11-20 15:59:34 -0300

    Commit inicial

---
+++ src/main/java/com/example/demo/JwtExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.bezkoder.com/spring-boot-jwt-authentication/
+
+@SpringBootApplication
+public class JwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtExampleApplication.class, args);
+	}
+
+}
