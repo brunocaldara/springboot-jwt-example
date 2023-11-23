8f82019cc115de0f0f8942eb5d4c20efe35dc234
Bruno Caldara <bruno@localhost.localdomain>
2023-11-23 09:09:05 -0300

    Adiciona link do github do projeto original

--- src/main/java/com/example/demo/JwtExampleApplication.java
+++ src/main/java/com/example/demo/JwtExampleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //https://www.bezkoder.com/spring-boot-jwt-authentication/
+//https://github.com/bezkoder/spring-boot-spring-security-jwt-authentication/tree/master
 
 @SpringBootApplication
 public class JwtExampleApplication {
